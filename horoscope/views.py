e44c74fdef926254cf57dd69e333f68d1d928fbf
maibenben <mr.mystory70@gmail.com>
2024-03-14 16:24:04 +0300

    создан урл для гороскоп + его представление

--- horoscope/views.py
+++ horoscope/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def leo(request):
+    return HttpResponse('<h3>Sign of zodiac "Leo"</h3>')
