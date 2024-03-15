ccb2023625ac2654d91c6987814661ac32f1294e
maibenben <mr.mystory70@gmail.com>
2024-03-15 17:01:04 +0300

    created urls for horoscope & week_days, and views for him

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('leo', views.leo),
-    path('scorpio', views.scorpio)
+    path('<int:num_zodiac>', views.get_ifo_sign_zodiac_by_num),
+    path('<str:sign_zodiac>', views.get_ifo_sign_zodiac),
 ]
--- horoscope/views.py
+++ horoscope/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 # Create your views here.
 
+zodiac_dict = {
+    'aries': 'Овен - первый знак зодиака, планета Марс (с 21 марта по 20 апреля).',
+    'taurus': 'Телец - второй знак зодиака, планета Венера (с 21 апреля по 21 мая).',
+    'gemini': 'Близнецы - третий знак зодиака, планета Меркурий (с 22 мая по 21 июня).',
+    'cancer': 'Рак - четвёртый знак зодиака, Луна (с 22 июня по 22 июля).',
+    'leo': ' Лев - <i>пятый знак зодиака</i>, солнце (с 23 июля по 21 августа).',
+    'virgo': 'Дева - шестой знак зодиака, планета Меркурий (с 22 августа по 23 сентября).',
+    '': 'Весы - седьмой знак зодиака, планета Венера (с 24 сентября по 23 октября).',
+    'scorpio': 'Скорпион - восьмой знак зодиака, планета Марс (с 24 октября по 22 ноября).',
+    'sagittarius': 'Стрелец - девятый знак зодиака, планета Юпитер (с 23 ноября по 22 декабря).',
+    'capricorn': 'Козерог - десятый знак зодиака, планета Сатурн (с 23 декабря по 20 января).',
+    'aquarius': 'Водолей - одиннадцатый знак зодиака, планеты Уран и Сатурн (с 21 января по 19 февраля).',
+    'pisces': 'Рыбы - двенадцатый знак зодиака, планеты Юпитер (с 20 февраля по 20 марта).',
+}
 
-def leo(request):
-    return HttpResponse('<h3>Sign of zodiac "Leo"</h3>')
 
-def scorpio(request):
-    return HttpResponse('<h3>Sign of zodiac "scorpio"</h3>')
+def get_ifo_sign_zodiac(request, sign_zodiac:str):
+    description = zodiac_dict.get(sign_zodiac)
+    if description:
+        return HttpResponse(f'<h2>Information about sign of zodiac "{sign_zodiac}:"<p>{description}</p>')
+    else:
+        return HttpResponseNotFound(f'<h4>Sign {sign_zodiac} is not exists</h4>')
+
+
+def get_ifo_sign_zodiac_by_num(request, num_zodiac:int):
+    zodiac_list = list(zodiac_dict)
+
+    if num_zodiac > len(zodiac_list):
+        return HttpResponseNotFound(f'<h3>Wrong sign zodiac:{name_zodiac}</h3>')
+    else:
+        name_zodiac = zodiac_list[num_zodiac -1]
+        return HttpResponseRedirect(f'{name_zodiac}')
