2ebfc0554211e91b5f3a136ac0a3b78b5ed94fc1
maibenben <70mr.mystory@gmail.com>
2024-03-16 18:30:49 +0300

    3.3 Django template language. Рендеринг шаблона.

--- horoscope/views.py
+++ horoscope/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 from django.template.loader import render_to_string
+
 # Create your views here.
 
 zodiac_dict = {
@@ -9,7 +10,7 @@ zodiac_dict = {
     'taurus': 'Телец - второй знак зодиака, планета Венера (с 21 апреля по 21 мая).',
     'gemini': 'Близнецы - третий знак зодиака, планета Меркурий (с 22 мая по 21 июня).',
     'cancer': 'Рак - четвёртый знак зодиака, Луна (с 22 июня по 22 июля).',
-    'leo': ' Лев - <i>пятый знак зодиака</i>, солнце (с 23 июля по 21 августа).',
+    'leo': ' Лев - пятый знак зодиака, солнце (с 23 июля по 21 августа).',
     'virgo': 'Дева - шестой знак зодиака, планета Меркурий (с 22 августа по 23 сентября).',
     '': 'Весы - седьмой знак зодиака, планета Венера (с 24 сентября по 23 октября).',
     'scorpio': 'Скорпион - восьмой знак зодиака, планета Марс (с 24 октября по 22 ноября).',
@@ -28,10 +29,13 @@ def index(request):
     ul_zodiac = f'<ul>{li_item}</ul>'
     return HttpResponse(ul_zodiac)
 
-
 def get_ifo_sign_zodiac(request, sign_zodiac: str):
-    return render(request, "horoscope/info_zodiac.html")
-
+    description = zodiac_dict.get(sign_zodiac)
+    data = {
+        'description': description,
+        'sign': sign_zodiac
+    }
+    return render(request, 'horoscope/info_zodiac.html', context=data)
 
 def get_ifo_sign_zodiac_by_num(request, num_zodiac:int):
     zodiac_list = list(zodiac_dict)
@@ -42,3 +46,5 @@ def get_ifo_sign_zodiac_by_num(request, num_zodiac:int):
         name_zodiac = zodiac_list[num_zodiac -1]
         redirect_url = reverse('zodiac', args=(name_zodiac,))
         return HttpResponseRedirect(redirect_url)
+
+
