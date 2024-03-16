92a7a9a13ccf98d616197a3ec4c765c8e020ca60
maibenben <70mr.mystory@gmail.com>
2024-03-16 10:40:01 +0300

    created index page with list(includes links)

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    path('', views.index),
     path('<int:num_zodiac>', views.get_ifo_sign_zodiac_by_num),
     path('<str:sign_zodiac>', views.get_ifo_sign_zodiac, name='zodiac'),
 ]
